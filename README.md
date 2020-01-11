6805d490f1e327f636a877aafe628d4265a205aa
Iskander Kamaliev <iskander.kamaliev@gmail.com>
2020-01-11 11:27:17 +0300

    Init

---
+++ README.md
@@ -0,0 +1,38 @@
+## Инструкция по установке
+
+В системе должен быть установлен `python3` и для работы с докер пакет `docker` так же должен быть установлен.
+
+В `python` скрипте используются пакеты `beautifulsoup4`, `requests`.
+
+Домены указываются в файле `domain.csv`, протокол подключения к доменам настраивается в`config.ini`.
+
+#### Установка с использованием Virtual env
+
+Введите в терминале:
+
+```bash
+python3 -m venv venv
+source ./venv/bin/active
+pip install beautifulsoup4
+pip install requests
+python main.py
+```
+
+#### Установка пакетов в систему
+
+Введите в терминале: 
+
+```bash
+pip3 install beautifulsoup4
+pip3 install requests
+python3 main.py
+```
+
+#### Запуск через Docker
+
+Введите в терминале:
+
+```bash
+docker build -t script .
+docker run -it --rm -v $(pwd):/usr/src/app script
+```
---
+++ main.py
@@ -0,0 +1,107 @@
+import requests
+import csv
+import configparser
+import random
+import re
+import logger
+from bs4 import BeautifulSoup
+from threading import Thread
+
+
+class MyThread(Thread):
+    """
+    Основной класс потока
+    """
+
+    def __init__(self, row, logger):
+        Thread.__init__(self)
+        self.row = row
+        self.logger = logger
+
+    def run(self) -> None:
+        domain = row.get('domain')
+        logger = self.logger
+
+        logger.info('Calling thread: {}'.format(domain))
+
+        try:
+            check(self.row)
+        except ConnectionError as err:
+            logger.error('{domain} - Connection error: {err}'.format(domain=domain, err=err))
+        except Exception as err:
+            logger.error('{domain} - Undefined error: {err}'.format(domain=domain, err=err))
+
+
+def check(row):
+    """
+    Проверка шагов оплаты
+    :param row:
+    :return:
+    """
+    with requests.session() as s:
+        url = '{protocol}://{domain}'.format(
+            protocol=config.get('default', 'protocol'),
+            domain=row.get('domain')
+        )
+
+        r = s.get(url)
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+        links = soup.select('a')
+        filter_links = []
+
+        for link in links:
+            if re.search(r'\/catalog\/Bestsellers\/', str(link.get('href'))):
+                filter_links.append(link.get('href'))
+
+        link = random.choice(filter_links)
+
+        r = s.get(link)
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+        links = soup.find_all('a')
+        filter_links = []
+
+        for link in links:
+            if re.search(r'\?buy=\d+', str(link.get('href'))):
+                filter_links.append(link.get('href'))
+
+        link = random.choice(filter_links)
+
+        r = s.get(link)
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+        form = soup.find('form', attrs={'id': 'checkoutForm'})
+        link = form.get('action')
+        textarea = soup.find('textarea', attrs={'id': 'resultArrC'})
+
+        r = s.post(link, data={
+            'resultArrC': textarea.text
+        })
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+        form = soup.find('form', attrs={'id': 'formOk'})
+        link = form.get('action')
+        textarea = soup.find('textarea', attrs={'name': 'resultArrC'})
+
+        r = s.post(link, data={
+            'resultArrC': textarea.text
+        })
+
+        if r.status_code == 200:
+            logger.info('{status} - {url}'.format(status=r.status_code, url=url))
+        else:
+            logger.warning('{status} - {url}'.format(status=r.status_code, url=url))
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    logger = logger.init_logger(__name__, testing_mode=False)
+
+    with open('domain.csv') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            thread = MyThread(row, logger)
+            thread.start()
